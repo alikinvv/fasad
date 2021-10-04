a354cdabc621a0f7d04768c0bc0df593f1b695dd
valikin <valikin@knoema.com>
2021-10-04 23:28:29 +0500

    update

---
+++ build/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+$('body').on('click', '.select__current', function (e) {
+  $(e.currentTarget).toggleClass('active');
+  $(e.currentTarget).parent().find('.select__dropdown').toggleClass('active');
+});
+$(document).on("click", function (e) {
+  if (jQuery(e.target).closest('.select').length === 0) {
+    $('.select__current').removeClass('active');
+    $('.select__dropdown').removeClass('active');
+  }
+});
+$(document).on('keyup', function (e) {
+  if (e.key == "Escape") {
+    $('.select__current').removeClass('active');
+    $('.select__dropdown').removeClass('active');
+  }
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-$('body').on('click', '', () => {});
+$('body').on('click', '.select__current', (e) => {
+    $(e.currentTarget).toggleClass('active');
+    $(e.currentTarget).parent().find('.select__dropdown').toggleClass('active');
+});
+
+$(document).on("click", function(e) {
+    if (jQuery(e.target).closest('.select').length === 0) {
+        $('.select__current').removeClass('active');
+        $('.select__dropdown').removeClass('active');
+    }
+});
+
+$(document).on('keyup', function(e) {
+    if (e.key == "Escape") {
+        $('.select__current').removeClass('active');
+        $('.select__dropdown').removeClass('active');
+    }
+});
